0573994237495d8d63d5f247c024db1bb633ff9d
kazu7610 <bpzcmczytn@privaterelay.appleid.com>
2026-08-23 20:11:41 +0900

    他会展開用クラブ設定ファイルを追加

---
+++ club-config.js
@@ -0,0 +1,12 @@
+window.CLUB_CONFIG = {
+  name: "ポンコツ倶楽部",
+  shortName: "ポンコツ倶楽部",
+
+  rulesFile: "club-rules.pdf",
+
+  appName: "山岳会ポータル",
+  appDescription: "山行管理専用ポータル",
+
+  establishedYear: 2021,
+  fiscalYearStartMonth: 4
+};
